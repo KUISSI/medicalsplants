faf91f473e965e6a9b29e1b0cf102e8bf3d354ab
KUISSI <gkuissi@gmail.com>
2026-03-14 22:19:52 +0100

    feat: update Java version to 21 and adjust related configurations; add nginx configuration for frontend and backoffice

--- frontend-app/src/environments/environment.prod.ts
+++ frontend-app/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.medicalsplants.com/api/v1',
+  apiUrl: '/api/v1',
   appName: 'Medicals Plants',
   tokenKey: 'mp_access_token',
-  refreshTokenKey:  'mp_refresh_token',
+  refreshTokenKey: 'mp_refresh_token',
   userKey: 'mp_user'
 };
